adf495a5f358fc8b8c126786b2d2f92043a14d13
gigulae <163216277+gigulae@users.noreply.github.com>
2024-06-19 23:39:06 +0900

    Add files via upload

---
+++ 과제/14주차 과제.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+void Eventhandler()
+{
+	cout << "으갸갸갸갸갸" << endl;
+}
+
+
+int main()
+{
+	void (*onCollisionEnter)();
+
+	onCollisionEnter = Eventhandler;
+
+	int count = 0;
+
+	chrono::steady_clock::time_point prev_end = chrono::steady_clock::now();
+	chrono::steady_clock::time_point end = chrono::steady_clock::now();
+
+	int delay_time_ms = 0;
+
+	while (count < 10)
+	{
+		this_thread::sleep_for(chrono::milliseconds(1000 + delay_time_ms));	
+		prev_end = end;
+		end = chrono::steady_clock::now();
+		chrono::steady_clock::duration diff = end - prev_end;
+		delay_time_ms = 1000 - chrono::duration_cast<chrono::milliseconds>(diff).count();
+		count++;
+		cout << count << delay_time_ms << endl;
+		onCollisionEnter();
+	}
+}
